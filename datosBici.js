05263937a64761fa9d8f289bd6467cf26c878a7c
sorrego98 <juan.sorrego98@gmail.com>
2023-05-24 21:25:44 -0500

    Actualización datosBici.js para leer figuras

--- datosBici.js
+++ datosBici.js
@@ -8,4 +8,21 @@ function devolverArray(){
 
 }
 
-module.exports = devolverArray;
+const urlsMarca = {
+    'Hot Toys': './datos/figuras1.json',
+    'Bandai': './datos/figuras2.json',
+    'Star Wars': './datos/figuras3.json'
+}
+
+const importar = marca => {
+    const url = urlsMarca[marca];
+    if(url === undefined){
+        return 'No se encontró la marca registrada'
+    }else{
+    const datos = fs.readFileSync(`${url}`, `utf-8`);
+    const info = JSON.parse(datos);
+    return info
+    }
+}
+
+module.exports = [{devolverArray, importar}];
